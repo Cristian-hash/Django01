f45e21331bc3df71259dcefb7f58e139a058e823
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-06-03 22:25:24 -0500

    Argumento de la funciones de las vistas

--- pro01/src/inicio/views.py
+++ pro01/src/inicio/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# from django.http import anotherView
 # Create your views here.
 
-def myHomeView(*args,**kwargs):
-    #
+def myHomeView(request,*args,**kwargs):
+    
+    print(args, kwargs)
+    print(request.user)
     return HttpResponse('<h1>Hola mundo desde Django</h1>')
 #    return render(request,"home.html",{})
 def anotherView(request):
