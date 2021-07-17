cb35981d025d9bd5236399acafe575517d6f36bb
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-07-17 12:07:55 -0500

    El contexto

--- pro01/src/inicio/views.py
+++ pro01/src/inicio/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 
 # from django.http import anotherView
 # Create your views here.
-
 #
 def myHomeView(request,*args,**kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse('<h1>Hola mundo desde Django</h1>')
-#    return render(request,"home.html",{})
+#   return HttpResponse('<h1>Hola mundo desde Django</h1>')
+#EL "CONTEXTO"
+    return render(request,"home.html",{})
 def anotherView(request):
     return HttpResponse('<h1>Solo otra pagina</h1>')
 #nuevas vistas
