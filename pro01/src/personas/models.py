ec1b1a07a43cd5e9dc3e5b18860740a4a52c7ea8
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-07-25 11:07:39 -0500

    Enlaces dinamicos de URL

--- pro01/src/personas/models.py
+++ pro01/src/personas/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Persona(models.Model):
     nombres=models.CharField(max_length=100)
-    apellidos=models.TextField(max_length=100)
+    apellidos=models.CharField(max_length=100)
     edad=models.IntegerField()
     donador=models.BooleanField()
+
+    def get_absolute_url(self):
+        return '/personas/' + str(self.id)+ '/'
